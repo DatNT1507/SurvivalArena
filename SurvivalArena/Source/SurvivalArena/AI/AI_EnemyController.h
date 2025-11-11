6258aee64bd53c0bad5acdfef8c339c1f0542098
DatNT1507 <168793285+DatNT1507@users.noreply.github.com>
2025-11-11 16:56:19 +0700

    Create Enemy, EnemyController

---
+++ SurvivalArena/Source/SurvivalArena/AI/AI_EnemyController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AI_EnemyController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVALARENA_API AAI_EnemyController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
